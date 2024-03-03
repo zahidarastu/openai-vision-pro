5933e2008089debd1afbb88aae2d05c0cafcc0ea
Zahid <zahid@kairosaerospace.com>
2024-03-03 10:18:56 -0800

    added multi gallery

--- NewVision/GalleryView.swift
+++ NewVision/GalleryView.swift
@@ -1,30 +1,66 @@
-//
-//  GalleryView.swift
-//  NewVision
-//
-//  Created by Zahid Husain on 3/3/24.
-//
-
 import Foundation
 import SwiftUI
 import RealityKit
 import RealityKitContent
 
+// Define a new struct for the first gallery
+struct GalleryOneView: View {
+    var body: some View {
+        VStack {
+            Text("Path One - San Francisco")
+                .font(.title)
+                .padding(.bottom, 10) // Adds some space between the title and the gallery
+            MixedContentGalleryView(contents: [
+//                .text("San Francisco"),
+                .image("SF-1"),
+                .image("SF-2"),
+                .image("SF-3"),
+                .image("SF-4"),
+            ])
+            Button("Continue Conversation with Future Self") {
+                print("Gallery One: Explore More button tapped.")
+            }
+            .padding(.top, 10)
+        }
+    }
+}
 
+// Define a new struct for the second gallery
+struct GalleryTwoView: View {
+    var body: some View {
+        VStack {
+            Text("Path Two - Singapore")
+                .font(.title)
+                .padding(.bottom, 10) // Adds some space between the title and the gallery
+            MixedContentGalleryView(contents: [
+//                .text("Singapore"),
+                .image("singapore-2"),
+                .image("singapore-1"),
+                .image("singapore-3"),
+                .image("singapore-4"),
+            ])
+            Button("Continue Conversation with Future Self") {
+                print("Gallery One: Explore More button tapped.")
+            }
+            .padding(.top, 10)
+        }
+    }
+}
+
+// Adjust the original GalleryView to include both galleries in separate panes
 struct GalleryView: View {
     var body: some View {
-        MixedContentGalleryView(contents: [
-            .image("Sunset-Zahid"),
-            .text("Pacifica Sunset in June."),
-            .image("Sutro-Tower"),
-            .text("A captivating story about Sutro Tower."),
-            .image("Waymo-burn"),
-            .text("Final thoughts on autonomous vehicles.")
-        ])
-
+        HStack {
+            GalleryOneView()
+                .frame(maxWidth: .infinity)
+                .padding()
+            GalleryTwoView()
+                .frame(maxWidth: .infinity)
+                .padding()
+        }
     }
 }
 
 #Preview(windowStyle: .automatic) {
-    GalleryView()
+    GalleryView().previewLayout(.sizeThatFits)
 }
