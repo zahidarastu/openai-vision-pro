81824741c92540176534721575d35048dd5693d9
zrizvi93 <zrizvi@atlassian.com>
2024-03-03 12:20:12 -0800

    more gallery

--- NewVision/GalleryView.swift
+++ NewVision/GalleryView.swift
@@ -3,46 +3,83 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+
 // Define a new struct for the first gallery
 struct GalleryOneView: View {
+    var audioPlayer = AudioPlayerUtil()
+    let fileName = "sharon_path1"
     var body: some View {
         VStack {
-            Text("Path One - San Francisco")
-                .font(.title)
-                .padding(.bottom, 10) // Adds some space between the title and the gallery
+            HStack{
+                Text("Path One - San Francisco")
+                    .font(.title)
+                Button(action: {
+                                audioPlayer.playSound(soundName: fileName, soundType: "mp3")
+                            }) {
+                                Image(systemName: "speaker.3")
+                            }
+                }.padding(.bottom, 10) // Adds some space between the title and the gallery
+            
             MixedContentGalleryView(contents: [
 //                .text("San Francisco"),
                 .image("SF-1"),
                 .image("SF-2"),
                 .image("SF-3"),
                 .image("SF-4"),
+                .image("SF-5"),
+                .image("SF-8"),
+                .image("SF-9"),
+                .image("SF-10"),
+                .image("SF-11"),
+                .image("SF-12"),
+                .image("SF-6"),
+                .image("SF-13")
             ])
-            Button("Continue Conversation with Future Self") {
-                print("Gallery One: Explore More button tapped.")
+            Button(action: {
+                            print("Gallery One: Explore More button tapped.")
+                        }) {
+                            Text("Continue Conversation with Future Self")
+                        }
+                        .padding(.top, 10)
             }
-            .padding(.top, 10)
         }
     }
-}
 
 // Define a new struct for the second gallery
 struct GalleryTwoView: View {
+    var audioPlayer = AudioPlayerUtil()
+    let fileName = "sharon_path2"
     var body: some View {
-        VStack {
-            Text("Path Two - Singapore")
+        VStack{
+            HStack {
+                Text("Path Two - Singapore")
                 .font(.title)
-                .padding(.bottom, 10) // Adds some space between the title and the gallery
+            Button(action: {
+                            audioPlayer.playSound(soundName: fileName, soundType: "mp3")
+                        }) {
+                            Image(systemName: "speaker.3")
+                        }
+            }.padding(.bottom, 10) // Adds some space between the title and the gallery
             MixedContentGalleryView(contents: [
 //                .text("Singapore"),
                 .image("singapore-2"),
                 .image("singapore-1"),
                 .image("singapore-3"),
                 .image("singapore-4"),
+                .image("singapore-5"),
+                .image("singapore-6"),
+                .image("singapore-7"),
+                .image("singapore-9"),
+                .image("singapore-10"),
+                .image("singapore-11"),
             ])
-            Button("Continue Conversation with Future Self") {
-                print("Gallery One: Explore More button tapped.")
-            }
-            .padding(.top, 10)
+            Button(action: {
+                            audioPlayer.playSound(soundName: fileName, soundType: "mp3")
+                            print("Gallery One: Explore More button tapped.")
+                        }) {
+                            Text("Continue Conversation with Future Self")
+                        }
+                        .padding(.top, 10)
         }
     }
 }
